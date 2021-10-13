13926de25b133fef84bd85d28450d3d729d07caa
mrbreenhd <eliteracerYT@gmail.com>
2021-10-13 17:48:48 +0100

    First Commit

---
+++ app/Http/Middleware/isAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::user() &&  Auth::user()->isAdmin == 1) {
+             return $next($request);
+        }
+
+        toastr()->error('You do not have permission to view that page.', 'Inconceivable!');
+        return redirect('home');
+    }
+} 
