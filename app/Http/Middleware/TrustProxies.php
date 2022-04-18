c5f8c0c2af82b074e3b6f3ba71e5d1334f2ef0ac
mrbreenhd <eliteracerYT@gmail.com>
2022-04-18 22:45:22 +0100

    Laravel 9 Upgrade

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
-use Fideloper\Proxy\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
@@ -19,5 +20,21 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers =    Request::HEADER_X_FORWARDED_FOR |    Request::HEADER_X_FORWARDED_HOST |    Request::HEADER_X_FORWARDED_PORT |    Request::HEADER_X_FORWARDED_PROTO |    Request::HEADER_X_FORWARDED_AWS_ELB;
+
+    /**
+     * Set the client as trusted
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $request->setTrustedProxies([$request->getClientIp()], $this->getTrustedHeaderNames());
+        $this->setTrustedProxyIpAddresses($request);
+
+        return $next($request);
+    }
 }
