43e5a411477ffa68e79f4f0a79a675e825280b5c
mrbreenhd <eliteracerYT@gmail.com>
2022-06-09 16:52:16 +0100

    can you tell that I am a backend developer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,121 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>SB Admin 2 - Login</title>
+        <!-- Custom fonts for this template-->
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+        <!-- Custom styles for this template-->
+        <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    </head>
+    <style>
+        body{
+            overflow: hidden; /* Hide scrollbars */
+        }
+        .bg-gradient-primary {
+            background: url('img/bg.jpg');
+            background-position: center;
+            background-size: cover;
+        }
+        .row {
+            max-height: 17em;
+            min-height: 16em;
+        }
+    </style>
+    <body class="bg-gradient-primary">
+        <div class="container">
+            <!-- Outer Row -->
+            <div class="row justify-content-center">
+                <div class="col-xl-10 col-lg-12 col-md-9">
+                    <div class="card o-hidden border-0 shadow-lg my-5">
+                        <div class="card-body p-0">
+                            <!-- Nested Row within Card Body -->
+                            <div class="row" style="">
+                                <div style="" class="col-lg-6 d-none d-lg-block">
+                                    <img style="max-width: 100%; max-height: 100%; position: relative;" src="img/ryr_logo.jpg" alt="">
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="p-5">
+                                        <div class="text-center">
+                                            <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        </div>
+                                        <a href="https://ryanair.rosterbuddy.org/">
+                                            <button type="submit" class="btn btn-primary btn-user btn-block">
+                                                Enter
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>                            
                         </div>
-                    @endif
-
-                    You are logged in!
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+        <br>
+        <div class="container">
+            <!-- Outer Row -->
+            <div class="row justify-content-center">
+                <div class="col-xl-10 col-lg-12 col-md-9">
+                    <div class="card o-hidden border-0 shadow-lg my-5">
+                        <div class="card-body p-0">
+                            <!-- Nested Row within Card Body -->
+                            <div class="row">
+                                <div style="" class="col-lg-6 d-none d-lg-block">
+                                    <img style="max-width: 100%; max-height: 100%; position: relative;" src="img/ein_logo.jpg" alt="">
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="p-5">
+                                        <div class="text-center">
+                                            <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        </div>
+                                        <a href="https://aerlingus.rosterbuddy.org/">
+                                            <button type="submit" class="btn btn-primary btn-user btn-block">
+                                                Enter
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>                         
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="container">
+            <!-- Outer Row -->
+            <div class="row justify-content-center">
+                <div class="col-xl-10 col-lg-12 col-md-9">
+                    <div class="card o-hidden border-0 shadow-lg my-5">
+                        <div class="card-body p-0">
+                            <!-- Nested Row within Card Body -->
+                            <div class="row">
+                                <div style="" class="col-lg-6 d-none d-lg-block">
+                                    <img style="max-width: 100%; max-height: 100%; position: relative; top: -78px;" src="img/unknown.png" alt="">
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="p-5">
+                                        <div class="text-center">
+                                            <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        </div>
+                                        <a href="https://pilotbuddy.rosterbuddy.org/">
+                                            <button type="submit" class="btn btn-primary btn-user btn-block">
+                                                Enter
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>                         
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
